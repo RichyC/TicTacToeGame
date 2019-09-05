e666d86c8f4083ae7bbba82de9d50b0e38e3e17d
Richy Castro <51010957+RichyC@users.noreply.github.com>
2019-09-05 18:13:12 -0400

    Update Player.java
    Shortened the code by deleting unnecessary object instantiations

--- Player.java
+++ Player.java
@@ -17,18 +17,24 @@ import javafx.scene.shape.Rectangle;
  * @author richycastro
  */
 public class Player {
+    //describes the players number in the tic tac toe game
     String playerNumber;
     
-    TicTacToeBoard ifWins= new TicTacToeBoard();
+    int numOfSquares= 0;
+       /*
+    Boolean variables will be used to detect whether a board space is marked with a player's "X" or "Y".
+    */
+    boolean canWin = false, winner = false, rec1Marked = false, rec2Marked = false, rec3Marked = false, rec4Marked = false, rec5Marked = false,
+    rec6Marked = false, rec7Marked = false, rec8Marked= false, rec9Marked = false;
 
     String returnText;
-
+    //constructor
     Player(String playerNumber){
 
         this.playerNumber= playerNumber;
 
     }
-
+    //sets the players mark for the Tic Tac Toe game
     void setMark(String mark){
 
         returnText= mark;
@@ -36,19 +42,83 @@ public class Player {
     }
 
     Text makeMove(Rectangle rec){
-
+        //creates text object that will be the players symbol for the game
         Text newSquare= new Text(returnText);
-
+        //customizes the font of the text to the games settings
         newSquare.setFont(Font.font("Courier", FontWeight.BOLD, FontPosture.REGULAR, 200));
 
         newSquare.setFill(Color.PURPLE);
-
+        /*
+        retrieves the rectangles which was clicked and its positioning
+        to set the players symbol at the same position
+        */
         newSquare.setX(rec.getX()+ 60);
 
         newSquare.setY(rec.getY()+150);
-
+        //returns the players symbol to replace the rectangle that was clicked on
         return newSquare;
 
+    }
+    
+    void searchForWin(){
+        
+        if(canWin== true){
+            
+            if((rec1Marked== true) && (rec2Marked == true) && (rec3Marked == true))
+                
+                winner= true;
+            
+            else if((rec4Marked == true) && (rec5Marked== true) && (rec6Marked== true)){
+                
+                winner= true;
+                
+            }
+            
+            else if((rec7Marked == true) && (rec8Marked == true) && (rec9Marked == true)){
+                
+                winner= true;
+            }
+                
+            else if((rec1Marked == true) && (rec4Marked== true)&& (rec7Marked== true)){
+                
+                winner= true;
+                
+            }
+            
+            else if((rec2Marked == true)&& (rec5Marked == true)&& (rec8Marked== true)){
+                
+                winner= true;
+                
+            }
+            
+            else if((rec3Marked == true)&& (rec6Marked == true)&& (rec9Marked == true)){
+                
+                winner= true;
+                
+            }
+            
+            else if((rec1Marked == true)&& (rec5Marked == true) && (rec9Marked == true)){
+                
+                winner= true;
+                
+            }
+            
+            else if((rec3Marked == true)&& (rec5Marked== true)&& (rec7Marked== true)){
+                
+                winner= true;
+                
+            }
+            
+        }
+        
+    }
+    
+    void canWinCheck(){
+        
+        if(numOfSquares >= 3)
+            
+        canWin =true;    
+        
     }
 
 }
